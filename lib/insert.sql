a38d97bda82e2eccd162d868e5d747b4ed1dc1e4
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-05 19:27:58 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO (name) VALUES ("Mr. Chocolate");
-INSERT INTO (name) VALUES ("Rowdy");
-INSERT INTO (name) VALUES ("Tabitha");
-INSERT INTO (name) VALUES ("Sergeant Brown");
-INSERT INTO (name) VALUES ("Melissa");
-INSERT INTO (name) VALUES ("Grinch");
-INSERT INTO (name) VALUES ("Wendy");
-INSERT INTO (name) VALUES (NULL);
+INSERT INTO students (name) VALUES ("Mr. Chocolate");
+INSERT INTO students (name) VALUES ("Rowdy");
+INSERT INTO students (name) VALUES ("Tabitha");
+INSERT INTO students (name) VALUES ("Sergeant Brown");
+INSERT INTO students (name) VALUES ("Melissa");
+INSERT INTO students (name) VALUES ("Grinch");
+INSERT INTO students (name) VALUES ("Wendy");
+INSERT INTO students (name) VALUES (NULL);
